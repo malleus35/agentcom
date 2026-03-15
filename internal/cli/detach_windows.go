640149a0454f6a1bd0a2013f238b266106668dca
malleus35 <malleus35@gmail.com>
2026-03-15 22:35:24 +0900

    feat(cli): add managed up and down commands
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ internal/cli/detach_windows.go
@@ -0,0 +1,20 @@
+//go:build windows
+
+package cli
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func configureDetachedProcess(cmd *exec.Cmd) error {
+	devNull, err := os.OpenFile(os.DevNull, os.O_RDWR, 0)
+	if err != nil {
+		return fmt.Errorf("cli.configureDetachedProcess: open devnull: %w", err)
+	}
+	cmd.Stdin = devNull
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+	return nil
+}
